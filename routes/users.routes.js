d4a39b057b350e70164188bfaf03d99a53b9a138
AndreaNaomi <kutaninaomi@gmail.com>
2022-10-03 10:30:04 -0300

    update1

--- routes/users.routes.js
+++ routes/users.routes.js
@@ -1,27 +1,8 @@
-const mongoose = require('mongoose');
-const Schema = mongoose.Schema;
+const express = require("express");
+const router = express.Router();
 
-const UserSchema = new Schema(
-    {
-      name: { type: String, required: true },
-      email: {
-        type: String,
-        required: true,
-        unique: true,
-        match: /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/,
-        age: { type: Number, required: true},
-        sexo: { type: String, required: true},
-        dataNasc: { type: Date, default: Date.now()},
-        orientacaoSexual: { type: String, required: true},
-        cidade: { type: String, required: true}
-      },
-      posts: [{ type: Schema.Types.ObjectId, ref: "Post" }],
-    },
-    {
-      timestamps: true
-    }
-  );
-  
-  const UserModel = mongoose.model("User", UserSchema);
-  
-  module.exports = UserModel;
+const UserModel = require("../models/User.model");
+
+
+
+module.exports = router;
